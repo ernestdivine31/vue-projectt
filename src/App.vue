f448285eb6013c542d0e5dcf9b199d954b5776e4
ernestdivine31 <ernestdivine31@gmail.com>
2023-03-23 15:31:49 +0100

    .

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,46 @@
   <div id="app">
     <Repositories />
   </div>
+    
+  
 </template>
 
 <script>
-import Repositories from './components/Repositories.vue';
-import '@/assets/tailwind.css';
+// import '@/assets/tailwind.css';
+import Repositories from '@/assets/pages/Repositories.vue';
 export default {
   name: 'App',
   components: {
-    Repositories,
+    Repositories
   },
 };
 </script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Sora:wght@400;600&display=swap');
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-size: inherit;
+}
+
+.wrapper {
+  max-width: 68.75rem;
+  margin: 0 auto;
+  width: calc(100% - 2rem);
+  border: 1px solid red;
+}
+@media screen and (min-width: 600px) {  
+  .wrapper {
+    width: calc(100% - 4rem);
+  }
+}
+body {
+  min-height: 100vh;
+  max-width:100%;
+  font-family: 'Sora', sans-serif;
+}
+
+</style>
---
+++ src/assets/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>
+            ERNETSDIVINE
+        </h1>
+    </div>
+</template>
+
+<script>
+
+</script>
